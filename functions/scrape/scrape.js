82ab990501af53587d2e814446f91b5cea7df274
KANAjetzt <kai@kana.jetzt>
2020-11-10 20:37:34 +0100

    🔨 wait 4 bot to login?!

--- functions/scrape/scrape.js
+++ functions/scrape/scrape.js
@@ -3,7 +3,7 @@ const request = require('request')
 const cheerio = require('cheerio')
 const {Client} = require('discord.js')
 
-exports.handler = async function(event, context) {
+ const handler = async (event, context) => {
   const client = new Client()
 
   const sendMessage = (message) => {
@@ -11,9 +11,10 @@ exports.handler = async function(event, context) {
     channel.send(message)
   }
 
-  client.login(process.env.DISCORDJS_BOT_TOKEN)
+ 
+  const token = await client.login(process.env.DISCORDJS_BOT_TOKEN)
 
-  client.on('ready', () => {
+  
     const url = 'https://www.notebooksbilliger.de/nvidia%252Bgeforce%252Brtx%252B3080%252Bfounders%252Bedition%252B683301?nbbct=1001_101248&nbb=2177fc&wt_cc8=adgoal&pid=11348_1603460418_79964a12f864b38ee6c28e1057c02b2a&awc=11348_1603460418_79964a12f864b38ee6c28e1057c02b2a'
     // const url = `https://www.notebooksbilliger.de/pc+hardware/grafikkarten/nvidia/geforce+rtx+3000+serie+nvidia/gigabyte+geforce+rtx+3090+vision+oc+24g+grafikkarte+685795`
     
@@ -44,7 +45,9 @@ exports.handler = async function(event, context) {
 
     return {
       statusCode: 200,
-      body: JSON.stringify({status: "success"})
+      body: JSON.stringify({status: "success", message: token})
   };
-  })
+  
 }
+
+exports.handler = handler

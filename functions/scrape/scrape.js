38b66b7e78ce5844295aff8b9e7a95a589fc23fb
KANAjetzt <kai@kana.jetzt>
2020-11-12 11:15:14 +0100

    🔨 just return?! 😮

--- functions/scrape/scrape.js
+++ functions/scrape/scrape.js
@@ -6,59 +6,53 @@ const {Client} = require('discord.js')
 
 const handler = async (event, context) => {
 
-  const sendMessage = (message) => {
-    const channel = client.channels.cache.find(channel => channel.name === '3080checker')
-    channel.send(message)
+  const scrape = (url) => {
+    return new Promise((resolve, reject) => {
+      request(url, async (error, response, html) => { 
+        if(!error & response.statusCode === 200) {
+          // load html in cheerio
+          const $ = cheerio.load(html)
+          // get availability_widget of NBB page
+          const availability = $('.availability_widget')
+          resolve(availability.text())
+        } else {
+          reject('error scraping')
+        }
+      })
+    })
   }
 
-  // RTX 3080 Founders Edition on Notebooksbilliger.de
-  const url = 'https://www.notebooksbilliger.de/nvidia%252Bgeforce%252Brtx%252B3080%252Bfounders%252Bedition%252B683301?nbbct=1001_101248&nbb=2177fc&wt_cc8=adgoal&pid=11348_1603460418_79964a12f864b38ee6c28e1057c02b2a&awc=11348_1603460418_79964a12f864b38ee6c28e1057c02b2a'
-  // const url = `https://www.notebooksbilliger.de/pc+hardware/grafikkarten/nvidia/geforce+rtx+3000+serie+nvidia/gigabyte+geforce+rtx+3090+vision+oc+24g+grafikkarte+685795`
-    
-    request(url, async (error, response, html) => {
-      
-      // On rquest error send back status 500 with error message
-      if(error){
-        return {
-          statusCode: 500,
-          body: JSON.stringify({status: "error", message: "error scraping"})
-      };
-      }
-      
-      // If no error
-      if(!error & response.statusCode === 200){
-        
-        // load html in cheerio
-        const $ = cheerio.load(html)
-        // get availability_widget of NBB page
-        const availability = $('.availability_widget')
-        
-        // If the text in the availability_widget doesn't include "ausverkauft"
-        if(!(availability.text()).includes('ausverkauft')){
-          try {
-            // send a message with the text to the discord channel
-            const client = new Client()
-            const token = await client.login(process.env.DISCORDJS_BOT_TOKEN)
-            sendMessage(availability.text())
-            // end send response 200 with success message
-            return {
-              statusCode: 200,
-              body: JSON.stringify({status: "success", message: token})
-          };
-          }catch(err) {
-            return {
-              statusCode: 500,
-              body: JSON.stringify({status: "error", message: "error sending discord message"})
-          };
-          }
-        } else {
-          return {
-            statusCode: 200,
-            body: JSON.stringify({status: "success", message: availability.text()})
-        };
-        }    
-      }
+  const sendMessage = async (message) => {
+    return new Promise(async (resolve, reject) => {
+      const client = new Client()
+      const token = await client.login(process.env.DISCORDJS_BOT_TOKEN)
+      client.on('ready', async () => {
+        const channel = await client.channels.cache.find(channel => channel.name === '3080checker')
+        const messageObj = await channel.send(message)
+        resolve(messageObj.content)
+      })
     })
+  }
+
+  const init = async () => {
+    // RTX 3080 Founders Edition on Notebooksbilliger.de
+    const url = 'https://www.notebooksbilliger.de/nvidia%252Bgeforce%252Brtx%252B3080%252Bfounders%252Bedition%252B683301?nbbct=1001_101248&nbb=2177fc&wt_cc8=adgoal&pid=11348_1603460418_79964a12f864b38ee6c28e1057c02b2a&awc=11348_1603460418_79964a12f864b38ee6c28e1057c02b2a'
+    const result = await scrape(url)
+    if(!result.includes('ausverkauft')) {
+      await sendMessage(result)
+      return {
+        statusCode: 200,
+        body: JSON.stringify({action: 'send Discord Message', message: result})
+    };
+    } else {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({action: 'nothing', message: result})
+    };
+    }
+  }
+
+  return await init()  
 }
 
 exports.handler = handler
